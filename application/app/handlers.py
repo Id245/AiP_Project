a84fbdabe9facfe0314a9e635f26ffeea3e01fd8
Sakartvelo's Disgrace <dimitryidem@gmail.com>
2024-10-22 22:05:01 +0300

    bot keyboard with function of showing all users in bd implemented.

--- application/app/handlers.py
+++ application/app/handlers.py
@@ -1,9 +1,15 @@
 from aiogram.types import Message
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import CommandStart
+import app.keyboards as kb
 
 router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message):
-    await message.answer('123')
+    await message.reply('123', reply_markup=kb.main)
+
+@router.message(F.text == '123')
+async def start(message: Message):
+    keyboard = await kb.reply_db_users()
+    await message.reply('It is not a cybercrime if you didnt get caught', reply_markup=keyboard)
